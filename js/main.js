8791975dac06c685ed60085928c3d62771939128
manhkt94 <iphone3gssyn@gmail.com>
2020-09-24 00:15:49 +0700

    enter

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // First Slider
 $(".slider-one").not(".slick-intialized").slick({
   autoplay: true,
-  autoplaySpeed: 2000,
+  autoplaySpeed: 3000,
   dots: true,
-  prevArrow: ".site-slider .slider-btn .prev",
-  nextArrow: ".site-slider .slider-btn .next",
+  // prevArrow: ".site-slider .slider-btn .prev",
+  // nextArrow: ".site-slider .slider-btn .next",
 });
 
 // Second Slider
